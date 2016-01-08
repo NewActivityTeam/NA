147041d99a3384cdf155f91f65e6e69a39dce789
smy <450152034@qq.com>
2016-01-08 17:01:29 +0800

    0.1.6	添加了service注释	db文件夹中存放数据库

---
+++ NewActivity/src/db/na.sql
@@ -0,0 +1,144 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : root
+Source Server Version : 50624
+Source Host           : localhost:3306
+Source Database       : na
+
+Target Server Type    : MYSQL
+Target Server Version : 50624
+File Encoding         : 65001
+
+Date: 2016-01-08 16:57:52
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tb_activity
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_activity`;
+CREATE TABLE `tb_activity` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(40) NOT NULL,
+  `description` varchar(5000) DEFAULT NULL,
+  `starttime` datetime DEFAULT NULL,
+  `endtime` datetime DEFAULT NULL,
+  `createtime` datetime NOT NULL,
+  `manager` varchar(255) NOT NULL,
+  `web_address` varchar(255) DEFAULT NULL,
+  `mobile_address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_assess
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_assess`;
+CREATE TABLE `tb_assess` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL,
+  `aid` bigint(20) NOT NULL,
+  `grade` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_comment`;
+CREATE TABLE `tb_comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL,
+  `aid` bigint(20) DEFAULT NULL,
+  `createtime` datetime NOT NULL,
+  `content` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comment_uid` (`uid`),
+  KEY `comment_aid` (`aid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_group
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_group`;
+CREATE TABLE `tb_group` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `aid` bigint(20) NOT NULL,
+  `groupname` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `leader` bigint(20) DEFAULT NULL,
+  `maxcount` int(11) DEFAULT '9999',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_group_apply
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_group_apply`;
+CREATE TABLE `tb_group_apply` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL,
+  `gid` bigint(20) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
+  `createtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_image
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_image`;
+CREATE TABLE `tb_image` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(500) DEFAULT NULL,
+  `aid` bigint(20) DEFAULT NULL,
+  `index` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_participation
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_participation`;
+CREATE TABLE `tb_participation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL,
+  `aid` bigint(20) NOT NULL,
+  `groupid` bigint(20) DEFAULT NULL,
+  `createtime` datetime NOT NULL,
+  `grade` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pcp_uid` (`uid`),
+  KEY `pcp_aid` (`aid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_userinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_userinfo`;
+CREATE TABLE `tb_userinfo` (
+  `uid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ybaccount` varchar(255) NOT NULL,
+  `authority` int(11) NOT NULL DEFAULT '0',
+  `height` float DEFAULT NULL,
+  `weight` float DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `sex` int(11) DEFAULT NULL,
+  `phonenumber` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_vote
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_vote`;
+CREATE TABLE `tb_vote` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(40) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
