72bb2a1590b45076205509943dbcfe88e21fb099
smy <450152034@qq.com>
2016-01-25 15:51:42 +0800

    2.0

--- NewActivity/src/com/na/controller/test/ActivityTestController.java
+++ NewActivity/src/com/na/controller/test/ActivityTestController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.openid4java.util.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
